7cf8702540aeb0cd347bba3b02e8ab61df348b46
Paige <prhoward97@gmail.com>
2022-10-19 15:42:13 -0400

    commit tree bf3f1499a92b127b930b18830a2249e573b3775a

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+const newobj = {
+    name: "Steve",
+    email: "steve@steve.com",
+}
+
+const requestBody = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(newobj),
+};
+function submitData() {
+    return fetch("http://localhost:3000/users", requestBody)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (newobj) {
+            console.log(newobj);
+        })
+        .catch(function (error) {
+            alert("Unauthorized Access");
+            console.log(error.message);
+        });
+    };
+
+ 
+
